b89c6ee4f017b1b182cbe09744391efa02ccdeb4
Mark Smith <aardmark.smith@gmail.com>
2025-06-12 12:13:27 +1000

    :sparkles: locations appear on map

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -2,8 +2,13 @@
 import { CENTER_MAGNETIC_ISLAND } from "~/lib/constants";
 
 const colorMode = useColorMode();
+const mapStore = useMapStore();
 const style = computed(() => colorMode.value === "dark" ? "/styles/dark.json" : "https://tiles.openfreemap.org/styles/liberty");
 const zoom = 8;
+
+onMounted(() => {
+  mapStore.init();
+});
 </script>
 
 <template>
@@ -13,5 +18,20 @@ const zoom = 8;
     :zoom="zoom"
   >
     <MglNavigationControl />
+    <MglMarker
+      v-for="point in mapStore.mapPoints"
+      :key="point.id"
+      :coordinates="[point.long, point.lat]"
+    >
+      <template #marker>
+        <div class="tooltip tooltip-top" :data-tip="point.label">
+          <Icon
+            name="tabler:map-pin-filled"
+            size="30"
+            class="text-secondary"
+          />
+        </div>
+      </template>
+    </MglMarker>
   </MglMap>
 </template>
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
-export const CENTER_MAGNETIC_ISLAND = [146.8517, -19.1551];
+import type { LngLatLike } from "maplibre-gl";
+
+export const CENTER_MAGNETIC_ISLAND = [146.8517, -19.1551] as LngLatLike;
